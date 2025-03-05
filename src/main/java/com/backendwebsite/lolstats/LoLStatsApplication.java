2c22f95bfb24f351494ee288b229dae26b75e5f2
Tendrun <mikisz2002@gmail.com>
2025-03-05 17:35:54 +0100

    init commit

---
+++ src/main/java/com/backendwebsite/lolstats/LoLStatsApplication.java
@@ -0,0 +1,13 @@
+package com.backendwebsite.lolstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoLStatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoLStatsApplication.class, args);
+    }
+
+}
