0f4a466d0c2d605d7e986084f66be9fe5540a9c2
Tendrun <mikisz2002@gmail.com>
2025-10-24 19:01:28 +0200

    chore/fix: fixing minro issues with eslint, css files, index file

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -6,7 +6,11 @@ import CampionDetail from "./pages/ChampionDetail/[index].tsx";
 import {
   QueryClient,
   QueryClientProvider,
-} from '@tanstack/react-query'
+} from '@tanstack/react-query';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
 
 const queryClient = new QueryClient()
 
@@ -28,5 +32,11 @@ function App() {
 
   );
 }
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
 
 export default App
